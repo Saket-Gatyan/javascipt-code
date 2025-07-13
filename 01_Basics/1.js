bc0d701f15d0448e30126754f18e2cc378838848
Saket Gatyan <gatyanrivo@gmail.com>
2025-07-13 01:36:11 +0000

    const and var

--- 01_Basics/1.js
+++ 01_Basics/1.js
@@ -1,1 +1,18 @@
-console.log("Hey Saket nice staying up late huh");
+const accountId = 144553
+let accountEmail="saket@google.com"
+var accountPassword="12345"
+accountCity="Bokaro"
+var accountState;
+//accountId = 2 . This is not allowed
+accountEmail="hxh@hx.com"
+accountPassword= "212121"
+accountCity="Ambala"
+
+
+console.log(accountId);
+
+/* Prefer not to use var 
+because of issue in block and functional scope
+*/
+
+console.table([accountId,accountPassword,accountCity,accountState])
