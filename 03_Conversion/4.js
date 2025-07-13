c97657cd3cc3617a9128302e90a19068f782e709
Saket Gatyan <gatyanrivo@gmail.com>
2025-07-13 16:03:14 +0000

    datatypes and operation part 2

--- 03_Conversion/4.js
+++ 03_Conversion/4.js
@@ -1,8 +1,24 @@
-console.log(2>1);
-console.log(2>=1);
-console.log(2<1);
-console.log(2 ==1);
-console.log(2!=1);
+// Type Convertion
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2 ==1);
+// console.log(2!=1);
 
 // console.log("2">1);
 // console.log("02">1);
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); //true
+// operators work a bit differently 
+// the reason is that an equality check == and comparsions > < >= <= work differently.
+//Comparisons convert null to a number, treating it as 0;
+//that's why null>=0 is true & null > 0 is false
+
+
+console.log(undefined==0);  // always false
+console.log(undefined>0);
+console.log(undefined>=0);
+
