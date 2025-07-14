4e227f7022bb0a664aaa6faa0a007b0a45c30964
Saket Gatyan <gatyanrivo@gmail.com>
2025-07-14 15:43:47 +0000

    memory and stack

---
+++ 04_stack&memory/5.js
@@ -0,0 +1,13 @@
+//++++++++++++++ Memory and Stack ++++++++++
+
+//Stack (primitive)  , heap(non-primitive)
+
+// stack works as call by value , generates a copy upon callling 
+
+let myYoutubename = "saketgatyandot.com"
+
+let anotherName = myYoutubename
+anotherName = "gatyanrivo"
+
+console.log(myYoutubename)
+console.log(anotherName)
