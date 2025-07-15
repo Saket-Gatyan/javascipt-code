10843063a25733fff4533735f01e8ff84fadb87a
Saket Gatyan <gatyanrivo@gmail.com>
2025-07-15 13:33:57 +0000

    stack and memory complete

--- 04_stack&memory/5.js
+++ 04_stack&memory/5.js
@@ -9,5 +9,17 @@ let myYoutubename = "saketgatyandot.com"
 let anotherName = myYoutubename
 anotherName = "gatyanrivo"
 
-console.log(myYoutubename)
-console.log(anotherName)
+// console.log(myYoutubename) //saketgatyandot.com
+// console.log(anotherName) //gatyanrivo
+
+let userOne = {
+    email : "user@gmail.com",
+    upi:"user@ybl"
+}
+// whereas heap works with the concept of call by reference
+let userTwo = userOne
+
+userTwo.email = "saketgatyan@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
