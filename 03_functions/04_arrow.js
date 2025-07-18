956f57269d4c5b4096aa024f4ec4530390f724e0
Saket-Gatyan <gatyanrivo@gmail.com>
2025-07-18 07:31:29 +0530

    EDITOR

--- 03_functions/04_arrow.js
+++ 03_functions/04_arrow.js
@@ -2,17 +2,17 @@ const user = {
     username : "Saket",
     price:999,
     welcomeMessage : function(){
-        console.log(`${this.username} , welcome to the website`)
+        console.log(`${this.username} , welcome to the website`)  // string interpolation
         console.log(this);
     }
-}
+}    // this --> keyword use to fetching according to the scope 
 
 // user.welcomeMessage()
 // user.username = "sam"
 // user.welcomeMessage()
 
-// console.log(this); // {} no context in global
-// in browser global object is window
+// console.log(this); // global scope is default , {} no context in global
+// in browser global object is window ,, thats why you can interact with browser
 
 // function chai(){
 //     let username = "saket"
@@ -20,16 +20,20 @@ const user = {
 // }
 
 // chai()
+
+
 // ()=>{} // arrow function
-const addTwo = (num1,num2)=>{
-    return num1+num2;  // explicit return
-}
-
-const addTwo = (num1,num2) => (num1+num2) //implicit declaraction
-
-const addTwo = (num1, num2) => ({username: "hitesh"})
 
 
-console.log(addTwo(3, 4))
+// const addTwo = (num1,num2)=>{
+//     return num1+num2;      // explicit return => we are writing the return statement
+// }
 
-console.log(addTwo(3,4)) //7
+// const addTwo = (num1,num2) => (num1+num2) //implicit declaraction
+
+// const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+// console.log(addTwo(3, 4))
+
+// console.log(addTwo(3,4)) //7
