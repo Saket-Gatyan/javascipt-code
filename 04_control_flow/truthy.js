0191f87018ac69680da4e4b46a0006f1837c09b6
Saket-Gatyan <gatyanrivo@gmail.com>
2025-07-18 20:01:09 +0530

    truthy and nullish ternary

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,10 +1,10 @@
-const userEmail = [0]
+const userEmail = []
 
-if (userEmail){
-    console.log("Got user email")
-}else{
-    console.log("Don't have user email") 
-}
+// if (userEmail){
+//     console.log("Got user email")
+// }else{
+//     console.log("Don't have user email") 
+// }
 
 // if saket@google.com => Got user email
 // if '' empty string => dont have user email
@@ -20,3 +20,40 @@ if (userEmail){
 // truty values
 
 // true ,1 , 1000 , "james",  "0" , 'false' , " " , [ ] , {}, function(){} (empty function),
+
+
+
+// if (userEmail.length == 0){
+//     console.log("Array is empty");
+
+// }
+
+
+//  
+
+// false == 0 => true , false == "" => true , 0=='' true
+
+
+
+// Nullish Coalescing Operator (??) : null , undefined
+
+let val1;
+// val1 = 5 ?? 10 ; // 5
+
+// val1 = null ?? 10 // 10
+
+// val1 = undefined ?? 15;  // 15
+
+val1 = nul ?? 10 ?? 20 // 10 , first value jo mile 
+
+
+
+console.log(val1);
+
+
+/// ++++++ Ternary Operator++++++++
+
+// condtion ? true : false;
+
+const iceTeaPrice =100
+iceTeaPrice >= 80 ? console.log("More than Eighty") : console.log("More than Eighty")
